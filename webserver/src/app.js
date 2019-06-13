5f5bd20a3239fae91cfe366981197c8e7aa0476a
Durga <dkanakaurga@gmail.com>
2019-06-13 17:02:39 +0100

    modified service

--- webserver/src/app.js
+++ webserver/src/app.js
@@ -24,6 +24,31 @@ app.get('', (req, res) => {
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+app.get('/messages', (req, res) => {
+    res.send([
+        {
+        senderName: "Den Dellon",
+        subject: "Need to remember to place the order",
+        gender: "male",
+        isOpen : "unread"
+        },
+        {
+            senderName: "Paige Turner",
+            subject: "Discount offer on recent purchase",
+            gender: "female",
+            isOpen : "read"
+        },
+        {
+            senderName: "Will Power",
+            subject: "Need to remember to place the order",
+            gender: "male",
+            isOpen : "read"
+        },
+
+
+])
+})
+
 
 
 
